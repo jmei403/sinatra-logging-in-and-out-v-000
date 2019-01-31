fe0105336e858358a51e0090d4ed3b5607b56525
Jason Mei <jason.mei403@gmail.com>
2019-01-31 18:37:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < Sinatra::Base
   get '/account' do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
+      binding.pry
       erb :account
     else
       erb :erb
